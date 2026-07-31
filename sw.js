55fd33fa0f549c98c6c432e9ae4a8a181c5e52a1
scrappymatt-arch <scrappymatt@hotmail.com>
2026-07-31 19:03:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,28 @@
-const CACHE = "matchlight-v1.2";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json", "icon.svg"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
-self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
-self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+const CACHE = "matchlight-v1.4";
+const ASSETS = ["./", "index.html", "styles.css?v=1.4", "app.js?v=1.4", "manifest.json", "icon.svg"];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
